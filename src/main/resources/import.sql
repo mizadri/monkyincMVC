729158f541cae028aa12b7026158c6750b071dc3
benny <benny@bennyPC>
2014-12-18 12:26:01 +0100

    import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,13 @@
-insert into Usuario values (0, '95ff31602486cecd7db623d4598a84b7aac60a5f', 'admin', 'admin', '37d602c186c2b7ea82750f0055b5ede5');
-insert into Usuario values (1, '95ff31602486cecd7db623d4598a84b7aac60a5f', 'user', 'user', '37d602c186c2b7ea82750f0055b5ede5');
-insert into Usuario values (2, '95ff31602486cecd7db623d4598a84b7aac60a5f', 'test', 'user', '37d602c186c2b7ea82750f0055b5ede5');
+insert into Usuario values (0, 'James','benito@correo.com','Calle menganito','95ff31602486cecd7db623d4598a84b7aac60a5f', 'admin','benito', 'admin', '37d602c186c2b7ea82750f0055b5ede5','600111222');
+insert into Usuario values (1, 'Palomares','pepito@correo.com','Calle coria','95ff31602486cecd7db623d4598a84b7aac60a5f', 'user', 'pepito','user', '37d602c186c2b7ea82750f0055b5ede5','610111222');
+insert into Usuario values (2, 'Andrade','juanito@correo.com','Calle perla','95ff31602486cecd7db623d4598a84b7aac60a5f', 'test', 'juanito','user', '37d602c186c2b7ea82750f0055b5ede5','620111222');
+INSERT INTO Pedido VALUES(1,2,60,0);
+INSERT INTO Pedido VALUES(2,1,35,1);
+INSERT INTO Pedido VALUES(3,2,90,2);
+INSERT INTO Pedido_Producto(1,2);
+INSERT INTO Pedido_Producto(2,1);
+INSERT INTO Pedido_Producto(3,3);
+INSERT INTO Producto(1,'camiseta',35,1);
+INSERT INTO Producto(2,'gorra',30,1);
+INSERT INTO Producto(3,'sudadera',45,1);
+INSERT INTO Tipo_producto(1,'ropa');
